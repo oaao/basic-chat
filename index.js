a7afac81aabe5dc4db5c637dec4719496bb9f2e7
Oliver Antoniu <oliver.antoniu@gmail.com>
2019-07-15 12:18:51 -0600

    add [sock]: socket.io integration

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var app  = require('express')();
 var http = require('http').createServer(app);
+var io   = require('socket.io')(http);
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', function(socket) {
+    console.log(Date.now() + ': a User has connected')
+});
+
 http.listen(3000, function() {
     console.log('Server listening on *:3000')
-})
+});
